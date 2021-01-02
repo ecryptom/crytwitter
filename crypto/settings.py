9c90494662aa53faef41e725792151be5cc32cde
mohamad <mr.mirshamsi.78@gmail.com>
2021-01-02 21:13:47 +0330

    add email to .env

--- crypto/settings.py
+++ crypto/settings.py
@@ -163,8 +163,8 @@ else:
 
 #email informations
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
