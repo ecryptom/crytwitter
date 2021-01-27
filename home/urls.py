e6dd57a282ec3035ea317c1be49cbeae48f6f57e
mohamad <mr.mirshamsi.78@gmail.com>
2021-01-27 14:53:48 +0330

    fix home currencies updater

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 
 
     ######  APIs  #######
-    path('get_all_currencies', get_all_currencies, name='get_all_currencies')
+    path('get_all_currencies', get_all_currencies, name='get_all_currencies'),
+    path('get_first_10_currency_info', get_first_10_currency_info, name='get_first_10_currency_info')
 ]
--- home/views.py
+++ home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import currency
+from .models import currency, dollor
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
 def home(req):
     currency.objects.get(symbol='LINK').id
-    return render(req, 'index.html', {'top_curs': currency.objects.filter(id__lt=currency.objects.get(symbol='USDC').id)})
+    return render(req, 'index.html', {'top_curs': currency.objects.filter(id__lt=currency.objects.get(symbol='USDC').id), 'dollor_rate':dollor.objects.get().rate})
 
 def cryptomarket(req):
     return render(req, 'cryptomarket.html')
@@ -23,4 +23,16 @@ def get_all_currencies(req):
         'persian_name' : cur.persian_name.decode(),
         'image_url' : cur.image_url
     } for cur in currency.objects.all()], safe=False)
+
+
+@csrf_exempt
+@login_required(login_url='login')
+def get_first_10_currency_info(req):
+    return JsonResponse([{
+        'symbol': cur.symbol,
+        'price': cur.price,
+        '1d_change': cur.daily_price_change_pct,
+        '7d_change': cur.weekly_price_change_pct
+    } for cur in currency.objects.all()[:10]], safe=False)
+
 
