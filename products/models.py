9125606c09a4828a1888d80870e494cc2979cb22
mohamad <mr.mirshamsi.78@gmail.com>
2021-04-17 19:45:23 +0430

    fix price format in templates

--- products/models.py
+++ products/models.py
@@ -41,7 +41,7 @@ class product(models.Model):
 
 
 class product_group(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     def __str__(self):
         return self.name   
     class Meta:
